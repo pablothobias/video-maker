f460e5e77d0bcf6d21833b1bf5739a3c40840874
pablothobias <pablo.thobias@gmail.com>
2019-04-01 16:18:34 -0300

    feat: adicionando google image search

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const robots = {
     input: require('./robots/input'),
     text: require('./robots/text'),
-    state: require('./robots/state')
+    state: require('./robots/state'),
+    image: require('./robots/image')
 }
 
 async function start() {
 
-    robots.input();
-    await robots.text();
+    // robots.input()
+    // await robots.text()
+    await robots.image()
 
     const content = robots.state.load()
 
-    console.dir(content, {depth: null});
+    console.dir(content, {depth: null})
 }
 
 start()
